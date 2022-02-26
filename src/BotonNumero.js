f403bf18dc03b886d74e0c90f7bf032767ddd07e
JuanMarGam <juan_jose.501@hotmail.com>
2022-02-26 14:28:44 -0500

    Entrega 1 Calculadora

---
+++ src/BotonNumero.js
@@ -0,0 +1,11 @@
+import { ACTIONS } from "./App"
+
+export default function BotonNumero({ dispatch, digit }) {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  )
+}
---
+++ src/BotonOpera.js
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./App"
+
+export default function BotonOpera({ dispatch, operation }) {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  )
+}
